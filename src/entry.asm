5dbc4deefeff46eabe5e11185aec78f95f3a8c2b
suida <suidar@foxmail.com>
2021-01-21 17:50:12 +0800

    Assemble project skeleton

---
+++ src/entry.asm
@@ -0,0 +1,18 @@
+	.section .text
+	.global _start
+_start:
+	la sp, boot_stack_top
+	call rust_main
+
+	# bss is a block if ELF, which only records length, since it will be initialized 
+	# as zero value.
+	# Here states the launch stack for os
+	.section .bss.stack
+	.global boot_stack
+boot_stack:
+	# Stack with size of 16 KB
+	.space 4096 * 16
+	.global boot_stack_top
+boot_stack_top:
+	# Stack end
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+
+#![no_main]
+
+#![feature(llvm_asm)]
+
+#![feature(global_asm)]
+
+#![feature(panic_info_message)]
+
+global_asm!(include_str!("entry.asm"));
+
+#[macro_use]
+mod console;
+mod panic;
+mod sbi;
+
+#[no_mangle]
+pub extern "C" fn rust_main() -> ! {
+    println!("Hello rCore-Tutorial!");
+    panic!("end of rust_main")
 }
